65d8f35064a4cbfa798d10149f1dd6a41a9289e7
Mozammel Haque <mozammel@livingoncodes.com>
2015-01-19 20:03:30 +0600

    Enabling Logger

--- src/main/java/com/livingoncodes/vboard/controller/HomeController.java
+++ src/main/java/com/livingoncodes/vboard/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.livingoncodes.vboard.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,8 +10,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(HomeController.class);
+
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String showHome() {
+		
+		LOGGER.debug("Rendering home view");
 		return "home";
 	}
 
